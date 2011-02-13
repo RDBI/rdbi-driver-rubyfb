ee3153ac2a41d0ce34b10f6005f23f0e475d3282
pilcrow <mjp@pilcrow.madison.wi.us>
2011-02-12 23:43:32 -0600

    Test txn failures due to open statements, zero-result statements

--- test/test_database.rb
+++ test/test_database.rb
@@ -65,15 +65,68 @@ class TestDatabase < Test::Unit::TestCase
     end
   end
 
+  def test_manual_commit
+    self.dbh = new_database
+
+    dbh.transaction do dbh.execute('drop table RUBYFB_TEST') rescue nil end
+    dbh.execute_modification('create table RUBYFB_TEST (I integer)')
+    assert_raise(Rubyfb::FireRubyException) do
+      # No such table (FB transactional DDL requires commit before DML)
+      dbh.execute_modification('insert into RUBYFB_TEST (I) values (?)', 1)
+    end
+
+    dbh.commit
+
+    assert_nothing_raised do
+      dbh.execute_modification('insert into RUBYFB_TEST (I) values (?)', 1)
+      dbh.commit
+    end
+
+    assert_nothing_raised do
+      dbh.execute_modification('drop table RUBYFB_TEST')
+      dbh.commit
+    end
+  end
+
+  def test_failed_commit
+    self.dbh = new_database
+
+    dbh.transaction do dbh.execute('drop table RUBYFB_TEST') rescue nil end
+    dbh.execute_modification('create table RUBYFB_TEST (I integer)')
+    dbh.commit
+
+    sth = dbh.prepare('insert into RUBYFB_TEST (I) values (?)')
+    sth.execute(1)
+
+    dbh.execute_modification('drop table RUBYFB_TEST')
+    assert_raises(Rubyfb::FireRubyException) do
+      # 'Object is in use' !
+      dbh.commit
+    end
+    sth.finish
+
+    assert_nothing_raised do
+      dbh.commit
+    end
+  end
+
   def test_execute
     self.dbh = new_database
 
-    assert_nothing_raised('Parameter-less bind throws no exceptions') do
+    assert_nothing_raised('Parameter-less bind raised an exception') do
       dbh.execute('SELECT 1 FROM RDB$DATABASE WHERE 1=0').finish
     end
 
-    assert_nothing_raised('Parameter bind throws no exceptions') do
+    assert_nothing_raised('Parameter bind raised an exception') do
       dbh.execute('SELECT 1 FROM RDB$DATABASE WHERE 1=?', 0).finish
     end
+  end
+
+  def test_zero_results
+    sth = dbh.prepare('insert into RUBYFB_TEST (I, VC) values (?, ?)')
+    res = sth.execute(6, 'sesa')
+    assert_equal(1, res.affected_count)
+    sth.finish
+    assert_equal(1, res.affected_count)
   end
 end

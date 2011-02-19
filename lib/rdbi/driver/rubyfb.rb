46a87e0de92638977931274ce5dad0007facf673
pilcrow <mjp@pilcrow.madison.wi.us>
2011-02-19 00:08:33 -0600

    Add preamble doc to RDBI::Driver::Rubyfb

--- lib/rdbi/driver/rubyfb.rb
+++ lib/rdbi/driver/rubyfb.rb
@@ -1,6 +1,12 @@
 require 'rdbi'
 require 'rubyfb'
 
+##
+#
+# RDBI database driver for the robust and SQL-compliant Firebird RDBMS, an
+# open-source member of the InterBase genus.  See the documentation for
+# RDBI::Driver::Rubyfb::Database for more information.
+#
 class RDBI::Driver::Rubyfb < RDBI::Driver
   def initialize(*args)
     super(Database, *args)
